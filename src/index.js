11be886d1dc6e4f69be3d2293d8419398338ded4
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-22 16:44:36 -0700

    clean up index, import reducer, remove unecc imports

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 import { createStore } from 'redux';
-import { ADD_DRINK, addDrink, ADD_CHIPS, addChips, ADD_SANDWICH, addSandwich } from './actions/lunchboxActions';
-import { REMOVE_DRINK, removeDrink, REMOVE_CHIPS, removeChips, REMOVE_SANDWICH, removeSandwich } from './actions/lunchboxActions';
-const initialState = {
-  drink: [],
-  sandwich: [],
-  chips: []
-};
+import { addDrink, addChips, addSandwich } from './actions/lunchboxActions';
+import { removeDrink, removeChips, removeSandwich } from './actions/lunchboxActions';
+import { lunchboxReducer } from './reducers/lunchboxReducer';
 
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case ADD_DRINK: 
-      return { ...state, drink: [...state.drink, action.payload] };
-    case REMOVE_DRINK: 
-      return { ...state, drink: state.drink.filter(item => item != action.payload) };
-    case ADD_SANDWICH: 
-      return { ...state, sandwich: [...state.sandwich, action.payload] };
-    case REMOVE_SANDWICH: 
-      return { ...state, sandwich: state.sandwich.filter(item => item != action.payload) };
-    case ADD_CHIPS: 
-      return { ...state, chips: [...state.chips, action.payload] };
-    case REMOVE_CHIPS: 
-      return { ...state, chips: state.chips.filter(item => item != action.payload) };
-    case 'EMPTY_BOX':
-      return { ...initialState };
-    case 'FILL_BOX': 
-      return {  
-        drink: [...state.drink, action.payload.drink],
-        sandwich: [...state.sandwich, action.payload.sandwich],
-        chips: [...state.chips, action.payload.chips]
-      };
-    
-    default: 
-      return state;
-  }
-}
-const store = createStore(reducer);
+const store = createStore(lunchboxReducer);
 
 store.subscribe(() => {
   const state = store.getState();
