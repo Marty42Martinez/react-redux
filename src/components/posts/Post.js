4fc16a6021adb95ddee1ab14a279a35d9057072c
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-23 15:02:26 -0700

    Post component

---
+++ src/components/posts/Post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Post({ post }) {
+  return (
+    <p>{post.title}</p>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default Post;
