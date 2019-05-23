42289422b0793a24b068e2c51b7e73c3c3a86e8a
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-23 15:25:19 -0700

    Posts component

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Post from './Post';
+
+function Posts({ posts }) {
+  const postList = posts.map((post, index) => {
+    return (
+      <li key={index}><Post post={post} /></li>
+    );
+  });
+  return (
+    <ul>
+      {postList}
+    </ul>
+  );
+}
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Posts;
