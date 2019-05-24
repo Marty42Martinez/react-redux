606ab9384a4503fdab48a83595cda5200c3bc745
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-24 16:01:56 -0700

    CommentForm container init

---
+++ src/containers/comments/CommentForm.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CommentForm extends PureComponent {
+  static propTypes = {
+
+  }
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
