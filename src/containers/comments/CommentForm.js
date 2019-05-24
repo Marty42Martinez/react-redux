75f4463e4a320060987127cc361085f833c187d1
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-24 16:34:48 -0700

    updateComment method

--- src/containers/comments/CommentForm.js
+++ src/containers/comments/CommentForm.js
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types';
 
 export default class CommentForm extends PureComponent {
   static propTypes = {
-
+    onSubmit: PropTypes.func.isRequired
   }
 
   state = {
+    comment: ''
+  }
 
+  updateComment = ({ target }) => {
+    this.setState({ comment: target.value });
   }
 
   render() {
+    const { comment } = this.state;
     return (
-      
+      <form onSubmit={this.handleSubmit}>
+        <textarea value={comment} onChange={this.updateComment}></textarea>
+        <button>Submit</button>
+      </form>
     );
   }
 }
