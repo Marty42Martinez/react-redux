8bfe4184ad70f7a0f3d541566b8881eb85dc057e
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-24 17:02:02 -0700

    CommentsByPostId, doing stuff

--- src/containers/comments/CommentsByPostId.js
+++ src/containers/comments/CommentsByPostId.js
@@ -3,7 +3,7 @@ import Comments from '../../components/comments/Comments';
 import { getComments } from '../../selectors/commentSelectors';
 
 const mapStateToProps = (state, { post_index }) => ({
-  comments: getComments(state, post_index);
+  comments: getComments(state, post_index)
 });
 
 export default connect(
